dad89308c0b4baf6577d58e47cd5aa284b536817
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-07 23:22:02 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  const superbowlWin= (array)=>{
+    const win=array.find(item=>item.result==="W") 
+    return win ? win.year: win
+  }
+    
+
+
+console.log(superbowlWin(record))
+  
